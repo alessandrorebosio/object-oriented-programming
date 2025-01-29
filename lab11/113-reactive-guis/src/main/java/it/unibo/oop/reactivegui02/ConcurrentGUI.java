0093bffaa5771cd351635ef24ea905d0faddc8b2
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-01-29 16:30:40 +0100

    added lab11 TODO

---
+++ lab11/113-reactive-guis/src/main/java/it/unibo/oop/reactivegui02/ConcurrentGUI.java
@@ -0,0 +1,14 @@
+package it.unibo.oop.reactivegui02;
+
+import javax.swing.JFrame;
+import java.io.Serial;
+
+/**
+ * Second example of reactive GUI.
+ */
+@SuppressWarnings("PMD.AvoidPrintStackTrace")
+public final class ConcurrentGUI extends JFrame {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
---
+++ lab11/113-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
@@ -0,0 +1,14 @@
+package it.unibo.oop.reactivegui03;
+
+import javax.swing.JFrame;
+import java.io.Serial;
+
+/**
+ * Third experiment with reactive gui.
+ */
+@SuppressWarnings("PMD.AvoidPrintStackTrace")
+public final class AnotherConcurrentGUI extends JFrame {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+}
